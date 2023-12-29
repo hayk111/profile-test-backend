435006de9a04b514e936fb82a595b6e575fb9268
hayk111 <hayk.atoyan111@gmail.com>
2023-12-29 19:43:10 +0400

    Test change

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,6 +3,7 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
+// test
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
